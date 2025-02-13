945e4b9b56998b2b5e79cb96812772d8ce3dfe1e
michin <niesmialymichal@gmail.com>
2025-02-13 17:39:40 +0100

    Playing with soft locks

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 #include <cstdint>
-#include "Errors.h"
+#include <thread>
+#include <Windows.h>
+#include "Errors.hpp"
 #include "TypePunning.hpp"
 
 
+static int a = 10;
+static CRITICAL_SECTION  critSec;
+
+void task(int i)
+{
+	//shows how threads are interupting eachother
+	std::cout << "my id: "<< i << " and a is: " << a << std::endl;
+	EnterCriticalSection(&critSec);
+	std::cout << i << " is incrementing" << std::endl;
+	++a;
+	std::cout << i << " finishing" << std::endl;
+	LeaveCriticalSection(&critSec);
+}
+
 int main()
 {
+	InitializeCriticalSection(&critSec);
+
 	STATIC_ASSERT(sizeof(int) == 4);
 	std::int32_t x = 128;
 	printf("0x%08x\n", x);
 	x = swapF32(x);
 	printf("0x%08x\n", x);
 
+	std::thread t[3];
+	for (int i = 0; i < 3; i++)
+	{
+		t[i] = std::thread(task, i);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		t[i].join();
+	}
+
+	DeleteCriticalSection(&critSec);
 }
